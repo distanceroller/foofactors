1cf30d467f01b3437a443c83e12519780ff5b883
distanceroller <frank.delatorre@gmail.com>
2021-05-24 18:05:11 +0000

    Primera prueba ejecutada
    La primera pruebs ha funcionado. Fallamos al medir la cobertura.

--- R/fbind.R
+++ R/fbind.R
@@ -1,4 +1,14 @@
 
+#' Bind two factors
+#'
+#' @param a factor
+#' @param b factor
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' fbind(iris$Species[c(1, 51, 101)], PlantGrowth$group[c(1, 11, 21)])
 fbind <- function(a, b) {
   factor(c(as.character(a), as.character(b)))
 }
